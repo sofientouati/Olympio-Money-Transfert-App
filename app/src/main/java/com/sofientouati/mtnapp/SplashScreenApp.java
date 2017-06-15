9208ace2e1460b5bc06c61f98afb8216df8c1454
sofien touati <sofientouati@gmail.com>
2017-06-15 19:17:18 +0100

    init project

---
+++ app/src/main/java/com/sofientouati/mtnapp/SplashScreenApp.java
@@ -0,0 +1,15 @@
+package com.sofientouati.mtnapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SplashScreenApp extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+    }
+}
